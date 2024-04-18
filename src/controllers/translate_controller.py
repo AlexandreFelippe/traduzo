60c99bf0c1f8fbb7697cc0d4a3d3a89b59be1099
AlexandreFelippe <alemfelippe@gmail.com>
2024-04-18 15:18:49 -0300

    Finaliza requisito 07

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -13,7 +13,7 @@ def translate():
         "text_to_translate": "O que deseja traduzir?",
         "translate_from": "pt",
         "translate_to": "en",
-        "translated": "What do you want to translate?"
+        "translated": "What do you want to translate?",
     }
     return render_template("index.html", **params)
 
@@ -26,13 +26,35 @@ def translate_post():
 
     translated = GoogleTranslator(
         source=translate_from, target=translate_to
-        ).translate(text_to_translate)
+    ).translate(text_to_translate)
 
     params = {
         "text_to_translate": text_to_translate,
         "translate_from": translate_from,
         "translate_to": translate_to,
-        "translated": translated
+        "translated": translated,
+    }
+
+    languages = LanguageModel.list_dicts()
+
+    return render_template("index.html", languages=languages, **params)
+
+
+@translate_controller.route("/reverse", methods=["POST"])
+def reverse_translate():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    reversed_text = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    params = {
+        "text_to_translate": reversed_text,
+        "translate_from": translate_to,
+        "translate_to": translate_from,
+        "translated": text_to_translate,
     }
 
     languages = LanguageModel.list_dicts()
