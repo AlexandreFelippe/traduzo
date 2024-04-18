18facb906e332abefaae7e1fc21f7b758298875a
AlexandreFelippe <alemfelippe@gmail.com>
2024-04-18 14:32:49 -0300

    Finaliza requisito 06

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 translate_controller = Blueprint("translate_controller", __name__)
 
@@ -15,3 +16,25 @@ def translate():
         "translated": "What do you want to translate?"
     }
     return render_template("index.html", **params)
+
+
+@translate_controller.route("/", methods=["POST"])
+def translate_post():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+        ).translate(text_to_translate)
+
+    params = {
+        "text_to_translate": text_to_translate,
+        "translate_from": translate_from,
+        "translate_to": translate_to,
+        "translated": translated
+    }
+
+    languages = LanguageModel.list_dicts()
+
+    return render_template("index.html", languages=languages, **params)
